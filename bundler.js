495ab3da009252ebde3914a725aeb47c15fff631
dz333333 <1714942752@qq.com>
2020-04-01 18:36:14 +0800

    update

--- bundler.js
+++ bundler.js
@@ -5,15 +5,18 @@ const traverse = require('@babel/traverse').default;
 const babel = require('@babel/core');
 
 const moduleAnalyser = (filename) => {
-
+    //利用node fs模块读取文件内容
     const content = fs.readFileSync(filename, 'utf-8');
+    //使用@babel/parser 分析文件内容生成AST
     const ast = parser.parse(content, {
         sourceType: 'module'
     });
-    // console.log(ast.program.body);
+    // console.log(ast);
+    //使用 @babel/traverse 分析AST
     const dependencies = {};
     traverse(ast, {
         ImportDeclaration({ node }) {
+            //利用 node path 拼接出依赖的完整路径
             const dirname = path.dirname(filename)
             const newFile = path.join(dirname, node.source.value)
             dependencies[node.source.value] = newFile
@@ -21,6 +24,7 @@ const moduleAnalyser = (filename) => {
     })
 
     // console.log(dependencies);
+    //利用@babel/core 解析import语句 ，并且 @babel/preset-env把es6语法转为浏览器可执行的es5
     const { code } = babel.transformFromAst(ast, null, {
         presets: ['@babel/preset-env']
     })
@@ -31,6 +35,8 @@ const moduleAnalyser = (filename) => {
     }
 
 }
+
+//遍历生成依赖图谱
 const makeDependenciesGraph = (entry) => {
     const entryModule = moduleAnalyser(entry);
     const graphArray = [entryModule];
@@ -47,28 +53,34 @@ const makeDependenciesGraph = (entry) => {
     const graph = {};
     graphArray.forEach((item) => {
         graph[item.filename] = {
-            dependencies: item.dependencies,
+            dependencies: item.dependencies,//{ './word.js': 'src\\word.js' }
             code: item.code
         }
     })
-    //  console.log(graphArray,'graph');
+     console.log(graphArray,'graph');
     return graph;
 }
 
 const generateCode = (entry) => {
     const graph = JSON.stringify(makeDependenciesGraph(entry));
-    // console.log(makeDependenciesGraph(entry));
-
+    //要先把对象转换为字符串，不然在下面的模板字符串中会默认调取对象的toString方法，参数变成[Object object]
+    console.log(graph);
+    
+    //定义require exports 以便浏览器执行
     return `
         (function(graph){
+            //第一层的require
             function require(module){
                 function localRequire(relativePath){
                     console.log(relativePath,module)
+                    //这里执行第一层的require方法
                     return require(graph[module].dependencies[relativePath])
                 }
                 var exports={};
                 (function (require,exports,code) {
                     eval(code);
+                    //执行eval(code)时,code里面还有依赖就继续执行require,
+                    //不过这个时候需要把相对路径转成绝对路径，就执行作为require参数名传进来的localRequire,完成路径转换后继续执行第一层的require
                 })(localRequire,exports,graph[module].code);
                 return exports;
             }
@@ -79,4 +91,4 @@ const generateCode = (entry) => {
 
 }
 const code = generateCode('./src/index.js');
-console.log(code);
+// console.log(code);
